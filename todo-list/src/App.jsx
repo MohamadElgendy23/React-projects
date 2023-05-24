5e8936b1a54cf574d8a9bd8a1928e92ce0829c69
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-05-23 19:25:15 -0600

    added files in the new repo. this is the repo in github so i should be able to push and pull now straight from vs code. :)

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,46 @@
+//App component -- component that main.js can render to create the UI
+import { useState } from "react"
+import './styles.css'
+
+export default function App() {
+  const [newItem, setNewItem] = useState("") //useState takes in initial value (in this case an empty string) for input text box
+  const [todos, setTodos] = useState([]); //gets the todos so far as an array
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos((currentTodos => {
+      return [
+        ...currentTodos, { id: crypto.getRandomUUID() }
+      ]
+    }));
+
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"></input>
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  )
+}
