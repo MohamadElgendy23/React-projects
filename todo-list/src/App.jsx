232eed82cd1ea96dbd8c707baeb6e2fb6a4c1842
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-05-24 23:27:00 -0600

    changed around setNewItem in order for it to not create an infinite loop when rendering every state. Line 19 to line 17 :)

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,42 +1,53 @@
 //App component -- component that main.js can render to create the UI
-import { useState } from "react"
-import './styles.css'
+import { useState } from "react";
+import "./styles.css";
 
 export default function App() {
-  const [newItem, setNewItem] = useState("") //useState takes in initial value (in this case an empty string) for input text box
-  const [todos, setTodos] = useState([]); //gets the todos so far as an array, starts off as an empty array 
+  const [newItem, setNewItem] = useState(""); //useState takes in initial value (in this case an empty string) for input text box
+  const [todos, setTodos] = useState([]); //gets the todos so far as an array, starts off as an empty array
 
   function handleSubmit(e) {
-    e.preventDefault();
-    setTodos(currentTodos => {
+    e.preventDefault(); //makes it so the page doesn't reload before
+    setTodos((currentTodos) => {
       return [
-        ...currentTodos, { id: crypto.randomUUID(), title: newItem, completed: false }
-      ]
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
     });
+    setNewItem("");
   }
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"></input>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          ></input>
         </div>
         <button className="btn">Add</button>
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        {todos.map(todo => {
+        {todos.map((todo) => {
           return (
-            <li>
+            <li key={todo.id}>
               <label>
-                <input type="checkbox" checked={todo.completed} />
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
                 {todo.title}
               </label>
               <button className="btn btn-danger">Delete</button>
             </li>
-          )
+          );
         })}
       </ul>
     </>
-  )
+  );
 }
