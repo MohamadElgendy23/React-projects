2407d0b03c8f501a63e4ebeabe70a13348054940
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-05-26 18:02:29 -0600

    added a selection box and a text box so that users can select the quiz topic and enter their name respectively

--- quizapp/src/StartQuizPage.jsx
+++ quizapp/src/StartQuizPage.jsx
@@ -1,8 +1,21 @@
 export default function StartQuizPage() {
   return (
     <>
-      <h1 id="QuizApp">Welcome to the Quiz!</h1>
+      <h1 id="QuizApp">Welcome to the Quiz App!</h1>
       <img src="https://www.icomedia.eu/wp-content/uploads/2017/06/Its-Quiz-Time-Logo.png"></img>
+      <form className="StartQuizForm">
+        <h1>Quiz Settings</h1>
+        <br></br>
+        <input id="EnterName" type="text" placeholder="Enter Your Name"></input>
+        <br></br>
+        <br></br>
+        <select id="Topic">
+          <option>Select Quiz Topic</option>
+          <option>Computer Science</option>
+          <option>Math</option>
+          <option>Geography</option>
+        </select>
+      </form>
     </>
   );
 }
