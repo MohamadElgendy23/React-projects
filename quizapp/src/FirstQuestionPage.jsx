724e7dffb8762ba089803e97e106beaecee40a9c
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-05-28 00:22:55 -0600

    created all the questions and got the question to display on firstquestionpage.

--- quizapp/src/FirstQuestionPage.jsx
+++ quizapp/src/FirstQuestionPage.jsx
@@ -1,4 +1,6 @@
-export default function FirstQuestionPage() {
-    
-  return <h1 id="Question1">Question 1</h1>;
+import QuizQuestions from "./QuizQuestions";
+
+//the first question page of the quiz app. contains the answers choices for the first question (that users can select) as well as the actual question.
+export default function FirstQuestionPage({ name, topic }) {
+  return <h1 id="Question1">{QuizQuestions[0][0].question}</h1>;
 }
--- quizapp/src/QuizQuestions.jsx
+++ quizapp/src/QuizQuestions.jsx
@@ -1,32 +1,89 @@
-export default function QuizQuestions() {
-  const CSQuestions = [
+//contains the questions for each topic as an array
+
+const QuizQuestions = [
+  //CS Questions
+  [
     {
       id: 1,
       question: "What data structure doesn't allow duplicates?",
-      questionChoices: ["Queue", "Map", "Set", "Array"],
+      answerChoices: ["Queue", "Map", "Set", "Array"],
       answer: "Set",
     },
     {
       id: 2,
       question: "What procudure do stacks follow?",
-      questionChoices: ["FIFO", "LIFO", "O(N)", "Plates"],
+      answerChoices: ["FIFO", "LIFO", "O(N)", "Plates"],
       answer: "LIFO",
     },
-    { id: 3, question: "What is the square root of 49" },
-    { id: 4, question: "What is the square root of 49" },
-  ];
+    {
+      id: 3,
+      question: "What procudure do queues follow?",
+      answerChoices: ["LIFO", "O(N)", "FIFO", "Push"],
+      answer: "FIFO",
+    },
+    {
+      id: 4,
+      question:
+        "What algorithm allows a splitting of numbers until a specific number is found?",
+      answerChoices: [
+        "Quick Sort",
+        "Selection Sort",
+        "Linear Search",
+        "Binary Search",
+      ],
+      answer: "Binary Search",
+    },
+  ],
 
-  const MathQuestions = [
-    { id: 1, question: "What is the square root of 49" },
-    { id: 2, question: "What is the square root of 49" },
-    { id: 3, question: "What is the square root of 49" },
-    { id: 4, question: "What is the square root of 49" },
-  ];
+  //Math Questions
+  [
+    {
+      id: 1,
+      question: "What is the square root of 49?",
+      answerChoices: ["6", "8", "7", "9"],
+      answer: "7",
+    },
+    {
+      id: 2,
+      question: "What is the integeral of x^2?",
+      answerChoices: ["x^3", "2x", "x^3/2", "x^3/3"],
+      answer: "x^3/3",
+    },
+    {
+      id: 3,
+      question: "What is the derivative of x^4+x^2+1?",
+      answerChoices: ["4x^3+2x", "4x^3", "2x", "x^5/5+x^3/3+x"],
+      answer: "4x^3+2x",
+    },
+    {
+      id: 4,
+      question: "If 3x-y=12, what is the value of 8^x/2^y?",
+      answerChoices: ["4^4", "8^2", "2^12", "Cant be determined"],
+      answer: "2^12",
+    },
+  ],
 
-  const GeographyQuestions = [
-    { id: 1, question: "What is the square root of 49" },
-    { id: 2, question: "What is the square root of 49" },
-    { id: 3, question: "What is the square root of 49" },
-    { id: 4, question: "What is the square root of 49" },
-  ];
-}
+  //Geography Questions
+  [
+    {
+      id: 1,
+      question: "What is the name of the tallest mountain in the world?",
+      answerChoices: ["Mount Everest", "Gasherbrum III", "Gyachung Kang", "K2"],
+      answer: "Mount Everest",
+    },
+    {
+      id: 2,
+      question: "What is the name of the largest country in the world?",
+      answerChoices: ["Canada", "United States", "Russia", "China"],
+      answer: "Russia",
+    },
+    {
+      id: 3,
+      question: "What is the only country that borders the United Kingdom?",
+      answerChoices: ["Ireland", "Ukraine", "Turkey", "USA"],
+      answer: "Ireland",
+    },
+  ],
+];
+
+export default QuizQuestions;
