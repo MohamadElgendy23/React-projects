6327a4923faa5e8c11c9e11ac1253c7bc0190080
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-05-28 09:57:38 -0600

    Added a cool border for displaying the name. now I will work on actually getting the name from the start page as well as the topic so i know which type of questions to ask.

--- quizapp/src/FirstQuestionPage.jsx
+++ quizapp/src/FirstQuestionPage.jsx
@@ -2,5 +2,10 @@ import QuizQuestions from "./QuizQuestions";
 
 //the first question page of the quiz app. contains the answers choices for the first question (that users can select) as well as the actual question.
 export default function FirstQuestionPage({ name, topic }) {
-  return <h1 id="Question1">{QuizQuestions[0][0].question}</h1>;
+  return (
+    <>
+      <h1 id="QuizApp">Welcome to the Quiz App!</h1>
+      <label className="WelcomeName">{"Welcome, " + name}</label>
+    </>
+  );
 }
