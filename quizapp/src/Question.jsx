9d666ad4e03d8ad5b1aec4a3e7514e4f2955fdff
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-05-28 19:20:22 -0600

    pushing for jasethemace hahahaa

--- quizapp/src/Question.jsx
+++ quizapp/src/Question.jsx
@@ -5,29 +5,38 @@ import { useState } from "react";
 export default function Question({ Questions, Category, ID }) {
   const [score, setScore] = useState(""); //keeps track of the score out of 4 (4 questions) for each topic.
   const [color, setColor] = useState("");
-  
-  const mapTopicToIndices = (topic) => { //maps topic/category to indices for easy array mapping
-    switch(topic) {
-        case("Computer Science"):
-            return 0;
-        case("Math"):
-            return 1;
-        case("Geography"):
-            return 2;
+
+  const mapTopicToIndices = (topic) => {
+    //maps topic/category to indices for easy array mapping
+    switch (topic) {
+      case "Computer Science":
+        return 0;
+      case "Math":
+        return 1;
+      case "Geography":
+        return 2;
     }
-  }
+  };
   //when user selects an answer out of the possible answers
   function handleAnswerSelect(event) {
-
-    if (event.target.value === Questions[mapTopicToIndices(Category)][ID-1].answer)
-    {
-        return "Correct";
+    if (
+      event.target.value ===
+      Questions[mapTopicToIndices(Category)][ID - 1].answer
+    ) {
+      return "Correct";
     }
     return "Wrong";
   }
 
-  return (<form className="QuestionForm" onSubmit={handleAnswerSelect}>
-    
-  </form>
+  return (
+    <form className="QuestionForm" onSubmit={handleAnswerSelect}>
+      <h1>Question {ID}:</h1>
+      <p>{Questions[mapTopicToIndices(Category)][ID - 1].question}</p>
+      {Questions.filter((question) => question[ID - 1].id === ID).map(
+        (question) => {
+          return <button>{question[ID - 1].answerChoices[key - 1]}</button>;
+        }
+      )}
+    </form>
   );
 }
