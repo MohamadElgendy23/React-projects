2d24cb6d606bafd5ad9dc61a49b1828c0a54185b
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-05-28 22:40:55 -0600

    question and questionpage looks much better now

--- quizapp/src/Question.jsx
+++ quizapp/src/Question.jsx
@@ -1,20 +1,6 @@
-import { useState } from "react";
-
 //this component represents each question in the UI. A question is formatted as such: question -> answer choices -> quit button and next
 
-export default function Question({ questionObj }) {
-  const [score, setScore] = useState(""); //keeps track of the score out of 4 (4 questions) for each topic.
-  //when user selects an answer out of the possible answers
-  function handleAnswerSelect(event) {
-    if (
-      event.target.value ===
-      Questions[mapTopicToIndices(Category)][ID - 1].answer
-    ) {
-      return "Correct";
-    }
-    return "Wrong";
-  }
-
+export default function Question({ questionObj, handleAnswerSelect }) {
   return (
     <div className="QuestionForm">
       <h1>Question {questionObj.id}:</h1>
--- quizapp/src/QuestionPage.jsx
+++ quizapp/src/QuestionPage.jsx
@@ -5,7 +5,29 @@ import Question from "./Question";
 //the question page of the quiz app. contains the User information and selected information as well as the question (from question.jsx)
 export default function QuestionPage({ Questions }) {
   const { name, category } = useParams(); //gets params from params passed into the path (format is :name && :category)
-  const [score, setScore] = useState(""); //keeps track of the score out of 4 (4 questions) for each topic.
+  const [score, setScore] = useState(0); //keeps track of the score out of 4 (4 questions) for each topic.
+  const [index, setIndex] = useState(0);
+  let questionObj = Questions[mapTopicToIndices(category)][index];
+
+  //when user selects an answer out of the possible answers
+  function handleAnswerSelect(event) {
+    if (event.target.value === questionObj.answer) {
+      setScore((score) => score + 1);
+    }
+    setIndex((index) => index + 1);
+  }
+
+  //maps topic/category to indices for easy array mapping
+  const mapTopicToIndices = (topic) => {
+    switch (topic) {
+      case "Computer Science":
+        return 0;
+      case "Math":
+        return 1;
+      case "Geography":
+        return 2;
+    }
+  };
 
   return (
     <>
@@ -13,7 +35,10 @@ export default function QuestionPage({ Questions }) {
       <label className="WelcomeName">{name}</label>
       <h1 id="SelectedTopic">Topic: {category} </h1>
       <h1 id="Score">Score: {score} </h1>
-      <Question questionObj={questionObj} />
+      <Question
+        questionObj={questionObj}
+        handleAnswerSelect={handleAnswerSelect}
+      />
     </>
   );
 }
