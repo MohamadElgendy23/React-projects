3db4b621c2144e03726e27fbd655325c0deee838
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-06-03 18:41:01 -0600

    havent looked at it much recently at all but atleast its done now. finished useEffect so now timer works properly (same as before) as well as correct logic inside usEffect.

--- quizapp/src/QuestionPage.jsx
+++ quizapp/src/QuestionPage.jsx
@@ -1,39 +1,38 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import Question from "./Question";
 import ScoreOutputPage from "./ScoreOutputPage";
+
+const STARTTIME = 5; //what second timer starts at
 //the question page of the quiz app. contains the User information and selected information as well as the question (from question.jsx)
 export default function QuestionPage({ Questions }) {
   const { category } = useParams(); //gets params fromparams passed into the path (format is :name && :category)
   const [score, setScore] = useState(0); //keeps track of the score out of 4 (4 questions) for each topic.
   const [index, setIndex] = useState(0); //keeps track of the index (question index in the array) (index: 0-3)
-  const [time, setTime] = useState(5); //5 seconds to answer each question
+  const [time, setTime] = useState(STARTTIME); //STARTTIME seconds to answer each question
 
   const name = localStorage.getItem("firstandlastname") ?? "";
   //timer logic
-  let intervalID; //for the specified timer interval, used for clearing intervals when needed.
-  //timer logic
-  const handleTimer = () => {
-    let timeRemaining = 5;
-    intervalID = setInterval(function () {
-      if (timeRemaining === 0) {
-        setTime(5);
-        setIndex((index) => index + 1);
-        //return early and clear interval if reached 0 seconds
-        return () => {
-          clearInterval(intervalID);
-        };
-      }
-      timeRemaining--;
-      setTime(timeRemaining);
-    }, 1000);
-    //clear timer when returned (clean up) =>
-    return () => {
-      clearInterval(intervalID);
-    };
-  };
+  let intervalID = useRef(null); //for the specified timer interval, used for clearing intervals when needed.
+  let timeRemaining = useRef(STARTTIME); //represents the time remaining for each "state"
 
-  useEffect(handleTimer, [index]); //reset the timer whenever the question changes
+  //timer logic
+  useEffect(() => {
+    setTime(STARTTIME);
+    timeRemaining.current = STARTTIME;
+    intervalID.current = setInterval(function () {
+      if (timeRemaining.current === 0) {
+        setIndex((index) => index + 1);
+        return;
+      }
+      timeRemaining.current--;
+      setTime(timeRemaining.current);
+    }, 1000);
+    //clear timer interval when returned (hook cleanup). runs before above code so that it clears the previous interval
+    return () => {
+      clearInterval(intervalID.current);
+    };
+  }, [index]);
 
   //when user selects an answer out of the possible answers
   const handleAnswerSelect = (event) => {
@@ -42,7 +41,6 @@ export default function QuestionPage({ Questions }) {
       setScore((score) => score + 1);
     }
     setIndex((index) => index + 1);
-    setTime(5);
   };
 
   let questionObj = Questions[category][index]; //each question (each object inside a topic array)
